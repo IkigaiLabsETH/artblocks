cfd275700f4bbfaf1c214cff9471fec53623dba9
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2023-07-05 14:59:28 +0200

    update

--- src/artcode/V1.js
+++ src/artcode/V1.js
@@ -1,9 +1,7 @@
 // ALOHA
 
 let shapes = [];
-const shapesNum = 10;
-
-const palette = ["#0276D1", "#011E60", "#79BCC9", "#F6F5BE", "#269182"];
+const shapesNum = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,6 +13,8 @@ function setup() {
 }
 
 function draw() {
+  // background("#E7ECF2");
+
   for (let i = 0; i < shapes.length; i++) {
     shapes[i].move();
     shapes[i].display();
@@ -29,26 +29,15 @@ class Shape {
     this.nx = random(1000);
     this.ny = random(1000);
     this.n = random(1000);
-    this.rotateSpeed = random(3, 10) * random([-1, 1]);
-    this.c = random(palette);
   }
 
   move() {
-    this.x = map(noise(this.n, this.nx), 0, 1, -width * 0.5, width * 1.5);
-    this.y = map(noise(this.n, this.ny), 0, 1, -height * 0.5, height * 1.5);
+    this.x = width * noise(this.n, this.nx);
+    this.y = height * noise(this.n, this.ny);
     this.n += 0.005;
   }
 
   display() {
-    fill(this.c);
-    push();
-    translate(this.x, this.y);
-    rotate(frameCount * this.rotateSpeed);
-    beginShape();
-    for (let i = 0; i < 360; i += 360 / 6) {
-      vertex(cos(i) * this.d, sin(i) * this.d);
-    }
-    endShape(CLOSE);
-    pop();
+    circle(this.x, this.y, this.d);
   }
 }
